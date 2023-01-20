babc1580f8160155d4242d1acdf0ae10c7b92063
farzadsnj <farzadsanjarani3@gmail.com>
2023-01-20 10:00:29 +0330

    co1

---
+++ src/scenes/Navbar.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import useMediaQuary from "../hooks/useMediaQuery";
+
+const Link = ({ page, selectedPage, setSelectedPage }) => {
+  const lowerCasePage = page.toLowerCase();
+  return (
+    <AnchorLink
+      className={`${
+        selectedPage === lowerCasePage ? "text-yellow" : ""
+      } hover: text-yellow transition duration-500`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+        {page}
+    </AnchorLink>
+  );
+};
+
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
+  const [isMenuToggled, setIsMediaToggled] = useState(false);
+  const isAboveSmallScreens = useMediaQuary("(min-width: 768px)");
+  return (
+    <nav className={`z-40 w-full fixed top-0 py-6`}>
+      <div className="flex items-center justify-between mx-auto w-5/6">
+        <h4 className="font-playfair text-3xl font-bold">FS</h4>
+
+        {isAboveSmallScreens ? (
+          <div className="flex justify-between gap-16 font-opensans font-semibold">
+            <Link page='Home' selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+            <Link page='Skills' selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+            <Link page='Projects' selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+            <Link page='Testimonials' selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+            <Link page='Contact' selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
