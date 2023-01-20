1c776f4f11a2afbd61fa5ca7ccebbc14457e55c4
farzadsnj <farzadsanjarani3@gmail.com>
2023-01-20 14:52:49 +0330

    co2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import useMediaQuary from './hooks/useMediaQuery'
 import Navbar from './scenes/Navbar'
 
 function App() {
   const [selectedPage, setSelectedPage] = useState('home')
+  const [isTopOfPage, setIsTopOfPage] = useState(true)
   const isAboveMediumScreens = useMediaQuary('(min-width: 1060px)')
+
+  useEffect(()=>{
+    const handleScroll = () => {
+      if(window.scrollY === 0) setIsTopOfPage(true)
+      else setIsTopOfPage(false)
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (
     <div className="app bg-deep-blue">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
   );
 }
